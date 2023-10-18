2db98499d4152dcdbb0648a4e348eb52363802c3
DracoYu <876319691@qq.com>
2023-10-18 16:33:33 +0800

    feat: 新增导出到EXCEL

---
+++ export_pkl_to_excel.py
@@ -0,0 +1,16 @@
+import config
+from utils import read_from_pickle
+import pandas as pd
+
+counter_data = read_from_pickle(config.counter_pickle_file_name)
+
+counter_data = {
+    "name": [item["name"] for item in counter_data],
+    "counter1": [item["counters"][0] for item in counter_data],
+    "counter2": [item["counters"][1] for item in counter_data],
+    "counter3": [item["counters"][2] for item in counter_data],
+    "counter4": [item["counters"][3] for item in counter_data],
+    "counter5": [item["counters"][4] for item in counter_data],
+}
+df = pd.DataFrame(counter_data)
+df.to_excel("output.xlsx", index=False)
